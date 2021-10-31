0b74a0388b9d968295faf28806d293f8d1d9bf79
yuhaoyann <yuhaoyann@gmail.com>
2021-10-30 23:51:58 -0400

    change getAllProperties function to fetch data from DB

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -1,13 +1,14 @@
-const properties = require("./json/properties.json");
-const users = require("./json/users.json");
-/// Users
 const { Pool } = require("pg");
 const pool = new Pool({
   user: "vagrant",
   password: "123",
   host: "localhost",
-  database: "bootcampx",
+  database: "lightbnb",
 });
+
+const properties = require("./json/properties.json");
+const users = require("./json/users.json");
+/// Users
 
 /**
  * Get a single user from the database given their email.
@@ -71,12 +72,23 @@ exports.getAllReservations = getAllReservations;
  * @param {*} limit The number of results to return.
  * @return {Promise<[{}]>}  A promise to the properties.
  */
-const getAllProperties = function (options, limit = 10) {
-  const limitedProperties = {};
-  for (let i = 1; i <= limit; i++) {
-    limitedProperties[i] = properties[i];
-  }
-  return Promise.resolve(limitedProperties);
+const getAllProperties = (options, limit = 10) => {
+  return pool
+    .query(
+      `
+    SELECT *
+    FROM properties
+    LIMIT $1
+    `,
+      [limit]
+    )
+    .then((result) => {
+      console.log(result.rows);
+      return result.rows;
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
 };
 exports.getAllProperties = getAllProperties;
 
