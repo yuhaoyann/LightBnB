0bf40ecbf77e83b6694e128936a47b13084ba272
yuhaoyann <yuhaoyann@gmail.com>
2021-10-31 18:29:26 -0400

    testing code

--- LightBnB_WebApp-master/hh.js
+++ LightBnB_WebApp-master/hh.js
@@ -1,3 +1,5 @@
-const { getUserWithEmail } = require("./server/database.js");
-getUserWithEmail("yuhaoyann@gmail.com").then((data) => console.log(data));
-// console.log(result);
+// const { getUserWithEmail } = require("./server/database.js");
+// getUserWithEmail("yuhaoyann@gmail.com").then((data) => console.log(data));
+// // console.log(result);
+var todayDate = new Date().toISOString().slice(0, 10);
+console.log(todayDate);
